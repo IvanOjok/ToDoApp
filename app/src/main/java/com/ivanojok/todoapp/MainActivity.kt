1f59d3b038442fb81d5eb8e9dc8059ef0892183d
OiJohnSteven <oisteven018@gmail.com>
2023-08-17 17:52:47 +0300

    Mainactivity

--- app/src/main/java/com/ivanojok/todoapp/MainActivity.kt
+++ app/src/main/java/com/ivanojok/todoapp/MainActivity.kt
@@ -16,34 +16,7 @@ class MainActivity : AppCompatActivity() {
         val binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val myList = binding.myList
-        val countryList = listOf("Uganda", "Kenya", "Tanzania", "Rwanda", "Burundi",
-            "DRC", "UK", "South Sudan", "USA", "Canada")
-        val cityList = listOf("Kampala", "Nairobi", "Dar e Salam", "Kigali", "Burundi",
-            "DRC", "UK", "South Sudan", "USA", "Canada")
-
-        //array adapter with single item view
-        //val arrayAdapter = ArrayAdapter(this, R.layout.list_item_layout, R.id.country_id, countryList)
-
-        //array adapter with multiple views
-        val arrayAdapter = NewAdapter(this, countryList, cityList)
-
-        myList.adapter = arrayAdapter
 
 
-
-    }
-}
-
-class NewAdapter(context:Context, val countryList: List<String>, val cityList: List<String>): ArrayAdapter<String>(context, R.layout.list_item_layout, R.id.country_id, countryList) {
-    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
-        val view =  super.getView(position, convertView, parent)
-
-        val countryId = view.findViewById<TextView>(R.id.country_id)
-        val cityId = view.findViewById<TextView>(R.id.city)
-        countryId.text = countryList[position]
-        cityId.text = cityList[position]
-
-        return view
     }
 }
